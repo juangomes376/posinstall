fd52de303bc444e15a9944b2289b359de0f1c3db
juangomes376 <juangomes376@gmail.com>
2022-03-27 18:14:54 +0200

    update

--- posinstall.sh
+++ posinstall.sh
@@ -12,31 +12,31 @@ sudo apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install yarn
+sudo apt install -y yarn 
 
 # instalando todos os programas via flatpak
-flatpak install flathub com.spotify.Client
-flatpak install flathub com.visualstudio.code
-flatpak install flathub io.github.Figma_Linux.figma_linux
-flatpak install flathub net.cozic.joplin_desktop
-flatpak install flathub org.onlyoffice.desktopeditors
-flatpak install flathub io.github.shiftey.Desktop
-flatpak install flathub org.telegram.desktop
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub org.kde.kdenlive
-flatpak install flathub me.kozec.syncthingtk
+flatpak install flathub -y com.spotify.Client
+flatpak install flathub -y com.visualstudio.code
+flatpak install flathub -y io.github.Figma_Linux.figma_linux
+flatpak install flathub -y net.cozic.joplin_desktop
+flatpak install flathub -y org.onlyoffice.desktopeditors
+flatpak install flathub -y io.github.shiftey.Desktop
+flatpak install flathub -y org.telegram.desktop
+flatpak install flathub -y com.discordapp.Discord
+flatpak install flathub -y org.kde.kdenlive
+flatpak install flathub -y me.kozec.syncthingtk
 
 # instalando unzip
-sudo apt install unzip
+sudo apt install -y unzip
 
 # baixando e instalando o vivalde 
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
 echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
 sudo apt update  
-sudo apt install vivaldi-stable
+sudo apt install -y vivaldi-stable
 
 # instalando e configurando docker + portainer
-sudo apt-get install \\
+sudo apt-get install -y \\
     apt-transport-https \\
     ca-certificates \\
     curl \\
@@ -50,7 +50,7 @@ sudo docker volume create portainer_data
 sudo docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer -H unix:///var/run/docker.sock
 
 # instalando o ohmyzsh/zinit
-sudo apt-get install dconf-cli
+sudo apt-get install -y dconf-cli
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://git.io/zinit-install)"
 zinit light zdharma/fast-syntax-highlighting
@@ -90,7 +90,7 @@ mkdir -p ~/.local/share/gnome-shell/extensions/bluetooth-quick-connect@bjarosze.
 cp -r * ~/.local/share/gnome-shell/extensions/bluetooth-quick-connect@bjarosze.gmail.com
 
 # net speed
-$ git clone https://github.com/AlynxZhou/gnome-shell-extension-net-speed.git ~/.local/share/gnome-shell/extensions/netspeed@alynx.one
+git clone https://github.com/AlynxZhou/gnome-shell-extension-net-speed.git ~/.local/share/gnome-shell/extensions/netspeed@alynx.one
 
 # Sound Input & Output Device Chooser
 cd ~/.local/share/gnome-shell/extensions/
@@ -130,9 +130,9 @@ cd
 
 # atualizacao final do sistema
 sudo apt-get update
-sudo apt-get full-upgrade
-sudo apt-get autoremove
-sudo apt-get autoclean
+sudo apt-get -y full-upgrade
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
 
 # reinicia  o sistema
 sudo reboot
