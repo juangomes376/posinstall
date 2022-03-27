f4ffdde925110deab6de43c461f81992e687bc88
juangomes376 <juangomes376@gmail.com>
2022-03-27 18:05:31 +0200

    Update posinstall.sh

--- posinstall.sh
+++ posinstall.sh
@@ -9,10 +9,10 @@ sudo apt-get update
 sudo apt-get install -y nodejs
 
 # instala o  yarn
-curl -sS <https://dl.yarnpkg.com/debian/pubkey.gpg> | sudo apt-key add -
-echo "deb <https://dl.yarnpkg.com/debian/> stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt-get install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install yarn
 
 # instalando todos os programas via flatpak
 flatpak install flathub com.spotify.Client
